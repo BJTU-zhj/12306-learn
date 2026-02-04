dc61218788eeaa2187d6b9d6936e10313f805778
haijian <haijianzhang2020@163.com>
2026-02-04 12:09:50 +0800

    增加主控页面

--- web/src/views/LoginView.vue
+++ web/src/views/LoginView.vue
@@ -41,11 +41,13 @@
 import { defineComponent, reactive } from 'vue';
 import axios from 'axios';
 import { notification } from "ant-design-vue";
+import { useRouter } from "vue-router";
+
 
 export default defineComponent({
   name: "login-view",
   setup() {
-
+    const router=useRouter();
     const loginForm = reactive({
       mobile: '13000000000',
       code: '',
@@ -72,6 +74,7 @@ export default defineComponent({
             let data = response.data;
             if(data.success){
               notification.success({description:'登录成功！'})
+              router.push("/")
             }else {
               notification.error({description:data.message})
             }
