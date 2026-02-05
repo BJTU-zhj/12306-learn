1be5485cfec48e6aeade66a22b1346b5d8dc6534
haijian <haijianzhang2020@163.com>
2026-02-05 21:51:50 +0800

    初步增加保存乘车人的接口以及校验等

---
+++ train-member/src/main/java/com/jiawa/train/member/enums/PassengerTypeEnum.java
@@ -0,0 +1,19 @@
+package com.jiawa.train.member.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PassengerTypeEnum {
+
+    ADULT("1", "成人"),
+    CHILD("2", "儿童"),
+    STUDENT("3", "学生");
+
+    private String code;
+    private String desc;
+
+    PassengerTypeEnum(String code, String desc){
+        this.code = code;
+        this.desc = desc;
+    }
+}
---
+++ train-member/src/main/java/com/jiawa/train/member/service/PassengerService.java
@@ -0,0 +1,28 @@
+package com.jiawa.train.member.service;
+
+import cn.hutool.core.date.DateTime;
+import com.jiawa.train.common.util.SnowUtil;
+import com.jiawa.train.member.DTO.PassengerSaveDTO;
+import com.jiawa.train.member.domain.Passenger;
+import com.jiawa.train.member.mapper.PassengerMapper;
+import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerService {
+
+    @Resource
+    private PassengerMapper passengerMapper;
+
+    //保存
+    public void save(PassengerSaveDTO passengerSaveDTO){
+        DateTime now=DateTime.now();
+        Passenger passenger = new Passenger();
+        BeanUtils.copyProperties(passengerSaveDTO,passenger);
+        passenger.setId(SnowUtil.getSnowflakeId());
+        passenger.setCreateTime(now);
+        passenger.setUpdateTime(now);
+        passengerMapper.insert(passenger);
+    }
+}
