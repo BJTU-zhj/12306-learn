4fcee4496fae77fff913358c2476cb799e63f20e
haijian <haijianzhang2020@163.com>
2026-02-07 16:18:43 +0800

    集成dom4j，读取xml文件。读取pom文件回去配置xml,获取table字段属性

---
+++ generator/src/main/java/com/jiawa/train/generator/gen/ServerGenerator.java
@@ -0,0 +1,43 @@
+package com.jiawa.train.generator.gen;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerGenerator {
+
+    static String toPath = "generator\\src\\main\\java\\com\\jiawa\\train\\generator\\test";
+    static String pomPath = "generator\\pom.xml";
+    static {
+        new File(toPath).mkdirs();
+    }
+
+    public static void main(String [] args) throws Exception {
+       String generatorPath =getGeneratorPath();
+        // 读取table节点
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+    }
+
+    //读取pom文件内的configurationFile节点获取生成器的xml文件位置
+    private static String getGeneratorPath() throws DocumentException {
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map=new HashMap<>();
+        map.put("pom","http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+        return node.getText();
+    }
+
+}
