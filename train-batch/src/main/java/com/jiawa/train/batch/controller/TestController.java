6637508e20b0acf5f2b1ec85700385727897d6ff
haijian <haijianzhang2020@163.com>
2026-02-14 19:17:49 +0800

    集成openfeign，实现组件之间的调用，给每个应用写应用名，进行hello测试

--- train-batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
+++ train-batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.jiawa.train.batch.controller;
 
+import com.jiawa.train.batch.feign.BusinessFeign;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    private BusinessFeign businessFeign;
+
     @GetMapping("/hello")
     public String hello(){
+        String hello =businessFeign.hello();
+        System.out.println(hello);
         return "hello world batch";
     }
 }
---
+++ train-batch/src/main/java/com/jiawa/train/batch/feign/BusinessFeign.java
@@ -0,0 +1,11 @@
+package com.jiawa.train.batch.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "train-business",url = "http://localhost:8002/business/")
+public interface BusinessFeign {
+
+    @GetMapping("hello")
+    public String hello();
+}
