570d2ec992c34f8daeb62ed7f241fba23851acc1
haijian <haijianzhang2020@163.com>
2026-02-25 22:00:42 +0800

    集成nacos配置中心，在member模块中使用nacos,并实现动态更新配置

--- train-member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ train-member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
@@ -15,10 +16,21 @@ public class MemberApplication {
 
 	private static final Logger LOG= LoggerFactory.getLogger(MemberApplication.class);
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(MemberApplication.class);
-		Environment env = app.run(args).getEnvironment();
+		// 1. 启动并获取 context（只启动这一次）
+		ConfigurableApplicationContext context = SpringApplication.run(MemberApplication.class, args);
+
+		// 2. 从 context 中获取 environment
+		Environment env = context.getEnvironment();
+
+		// 3. 打印信息
 		LOG.info("启动成功！！");
-		LOG.info("地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
+		LOG.info("地址: \thttp://127.0.0.1:{}{}/hello",
+				env.getProperty("server.port"),
+				env.getProperty("server.servlet.context-path"));
+
+		// 打印出当前环境识别到的应用名
+		String appName = env.getProperty("spring.application.name");
+		System.out.println("当前识别到的应用名是：" + appName);
 	}
 
 }
