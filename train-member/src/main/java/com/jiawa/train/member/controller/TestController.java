b14ee38539936b7822b6896c3d21116112c6ed83
haijian <haijianzhang2020@163.com>
2026-02-25 23:12:12 +0800

    gateway+nacos实现通过应用名字进行路由转发

--- train-member/src/main/java/com/jiawa/train/member/controller/TestController.java
+++ train-member/src/main/java/com/jiawa/train/member/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.jiawa.train.member.controller;
 
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,11 @@ public class TestController {
     @Value("${test.zhj}")
     private String nacosConfig;
 
+    @Resource
+    private Environment environment;
+
     @GetMapping("/test")
     public String test() {
-        return nacosConfig;
+        return "配置："+ nacosConfig+","+"端口："+ environment.getProperty("local.server.port");
     }
 }
